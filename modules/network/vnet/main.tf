9aac048c7c0583f1764a195a7510f7f3eab35bdd
unknown <mdshahilofficial@gmail.com>
2026-08-25 09:10:00 +0530

    validation error fixed

--- modules/network/vnet/main.tf
+++ modules/network/vnet/main.tf
@@ -5,5 +5,5 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
   address_space       = each.value.address_space
-  tags                = lookup(each.key, "tags", null)
+  tags                = lookup(each.value, "tags", null)
 }
